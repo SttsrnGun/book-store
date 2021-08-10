10c87d3f6b76e31698ce2773248c37548ecf797c
Sittisorn <sittisorn.k@mde.co.th>
2021-08-11 00:16:50 +0700

    initial

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+    
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        // $2y$13$56f7QcBgK2DKm4ntV6.MjO7byHEGVYf6VwNCxuT2Ympo9sxmpdIdi
+        $user = new User();
+        $user->setEmail('test@email.com');
+        $user->setRoles($user->getRoles());
+        $user->setPassword($this->passwordHasher->hashPassword(
+            $user,
+            'the_new_password'
+        ));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
