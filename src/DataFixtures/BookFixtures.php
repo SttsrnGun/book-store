d87b8f40e88bb6894a6407172be260dc695f324f
Sittisorn <sittisorn.k@mde.co.th>
2021-08-14 20:51:04 +0700

    add: book entity, fixture(mocking up from json)

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -10,18 +10,48 @@ class BookFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        
-        for ($i=0; $i < 20; $i++) { 
+        $bookData = file_get_contents('src\DataFixtures\bookData.json');
+        $bookList = json_decode($bookData, true);
+        // var_dump($bookData); // show contents
+        foreach ($bookList as $item) {
             $book = new Book();
-            $book->setName('Book'.$i);
-            $book->setDetail(
-                $i.
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                .$i
-            );
-            $book->setPrice($i+20);
+            $book->setName($item['title']);
+            $book->setDetail($item['summary']);
+            $book->setPrice($item['price']['displayValue']*33);
+            $book->setAbout($item['author']);
+            $book->setImagePath($item['image']);
             $manager->persist($book);
         }
+        // for ($i=0; $i < 20; $i++) { 
+        //     $book = new Book();
+        //     $book->setName('ฟื้นคืน REVIVAL'.$i);
+        //     $book->setDetail(
+        //         $i.
+        //         "ในเมืองเล็กๆ แห่งหนึ่ง เจมี มอร์ตัน เด็กชายตัวน้อย
+        //         ได้พบกับ ชาร์ลส์ เจค็อบส์ สาธุคุณคนใหม่ของเมืองโดยบังเอิญ
+        //         ทั้งสองคนสนิทสนมกันอย่างรวดเร็ว
+        //         เพราะชื่อชอบการทดลองกระแสไฟฟ้าเหมือนกัน
+        //         จนกระทั่งวันหนึ่งเกิดอุบัติเหตุอันน่าเศร้าขึ้นทำให้ทุกอย่างพลิกผัน
+                
+        //         หลายปีต่อมา ขณะที่เจมีเป็นนักดนตรี เขาได้เจอกับเจค็อบส์อีกครั้ง
+        //         ทว่าการพบกันครั้งนี้กลับมีความหมายมากกว่าที่คิด
+        //         ความผูกพันของพวกเขากลายเป็นพันธสัญญา
+        //         ที่ร้ายกาจยิ่งกว่าแผนการของปีศาจ
+        //         และเขาจะทำอย่างไรเมื่อสิ่งที่รออยู่เบื้องหลัง การฟื้นคืน นั้น
+        //         กลับกลายเป็นความหวาดผวาเกินต้านทาน!"
+        //         .$i
+        //     );
+        //     $book->setPrice(499);
+        //     $book->setAbout(
+        //         $i.
+        //         "สตีเวน คิง คือนักเขียนชื่อดังที่ทุกคนต่างรู้จัก และเรื่อง ฟื้นคืน หรือ Revival ที่อยู่ในมือทุกท่านนี้ ก็คือผลงานที่ผู้อ่านทั่วโลกลงความเห็นว่าเป็นงานที่ดีที่สุดเรื่องหนึ่งของคิง
+        //         เมื่อคนที่ศรัทธาในพระเจ้ากลับต้องสูญเสียครอบครัวอันเป็นที่รักยิ่งไปตลอดกาล ความศรัทธาจึงแปรเปลี่ยนเป็นความชิงชัง สตีเวน คิง ทำให้ผู้อ่านได้เห็นว่าความสิ้นหวังที่แท้จริงเป็นอย่างไร รวมทั้งพาทุกคนเข้าสู่ใจกลางของคำถามสำคัญที่ว่าโลกหลังความตายมีอยู่จริงหรือไม่                
+        //         แพรวสำนักพิมพ์"
+        //         .$i
+        //     );
+        //     $book->setImagePath('https://storage.naiin.com/system/application/bookstore/resource/product/202106/528559/1000241701_front_XXXL.jpg?imgname=%E0%B8%9F%E0%B8%B7%E0%B9%89%E0%B8%99%E0%B8%84%E0%B8%B7%E0%B8%99-REVIVAL');
+        //     $manager->persist($book);
+        // }
         
         $manager->flush();
     }
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -4,11 +4,19 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=BookRepository::class)
+ * @ApiFilter(PropertyFilter::class, arguments={
+ *      "parameterName": "properties", 
+ *      "overrideDefaultProperties": false, 
+ * })
  */
 class Book
 {
@@ -33,6 +41,26 @@ class Book
      * @ORM\Column(type="float", nullable=true)
      */
     private $price;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $about;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imagePath;
+
+    /**
+     * @ORM\OneToMany(targetEntity=ReviewBook::class, mappedBy="book")
+     */
+    private $reviewBooks;
+
+    public function __construct()
+    {
+        $this->reviewBooks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -71,6 +99,77 @@ class Book
     public function setPrice(?float $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAbout(): ?string
+    {
+        return $this->about;
+    }
+
+    public function setAbout(?string $about): self
+    {
+        $this->about = $about;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(?string $imagePath): self
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ReviewBook[]
+     */
+    public function getReviewBooks(): Collection
+    {
+        return $this->reviewBooks;
+    }
+
+    public function getAverageReviewScore(): ?float
+    {
+        $count = 0.0;
+        for ($i = 0; $i < count($this->getReviewBooks()); $i++) {
+            $count = $count + $this->getReviewBooks()[$i]->getscore();
+        }
+        if(count($this->getReviewBooks())){
+            return $count/count($this->getReviewBooks());
+        }
+        return 0;
+    }
+
+    public function getReviewCount(): ?int
+    {
+        return count($this->getReviewBooks());
+    }
+
+    public function addReviewBook(ReviewBook $reviewBook): self
+    {
+        if (!$this->reviewBooks->contains($reviewBook)) {
+            $this->reviewBooks[] = $reviewBook;
+            $reviewBook->setBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReviewBook(ReviewBook $reviewBook): self
+    {
+        if ($this->reviewBooks->removeElement($reviewBook)) {
+            // set the owning side to null (unless already changed)
+            if ($reviewBook->getBook() === $this) {
+                $reviewBook->setBook(null);
+            }
+        }
 
         return $this;
     }
